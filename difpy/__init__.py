dbe247c505d32f7afe9824038d0afb2f64acde60
Smith <bartlomiejczajewski@gmail.com>
2019-11-13 22:11:11 +0100

    add init file

---
+++ difpy/__init__.py
@@ -0,0 +1,14 @@
+# name of the package
+name = "difpy"
+
+
+# Enable to import functions directly from difpy 
+# for egzample "import function from difpy"
+from .difpy import (
+  function_1,
+  function_2
+)
+
+# or
+#from file1 import *
+#from file2 import *
